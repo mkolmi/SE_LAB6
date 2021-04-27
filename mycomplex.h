ea4a032d2fef77f40ce73fd40ffd4a69298ff018
Kolmichevskiy Vlasov <mkolmi@mail.ru>
2021-04-28 00:29:49 +0500

    File mycomplex.h with comments for Doxygen

--- mycomplex.h
+++ mycomplex.h
@@ -3,28 +3,67 @@
 #include <iostream>
 using namespace std;
 
+/** @brief Creating class "Complex" of Complex Value
+*@param aRe - Real part of complex value
+*@param aIm - Imagine part of complex value
+*/
 class Complex {
+  /**@param Re - Real part of complex value
+  */
   double Re;
+  /**@param Im - Imagine part of complex value
+  */
   double Im;
   public:
+    	/**@brief constructor for Complex Value class 
+     	*/
 	Complex(double aRe = 0, double aIm = 0);
 	Complex(const Complex &);
+	/**@brief destructor for Complex Value class 
+	*/
 	~Complex();
 	void Set(double aRe, double aIm = 0);
 	operator double();
+	/**@return Return abs of complex value
+	*/
 	double abs();
+	/**@brief Friend Operator for inserting complex value in stream
+   	*/
 	friend istream & operator >> (istream &, Complex &);
+	/**@brief Friend Operator for getting complex value from stream
+    	*/
 	friend ostream & operator << (ostream &, Complex &);
+	/**@brief Operator sums one complex value with other
+	*/
 	Complex operator + (const Complex &);
+	/**@brief Operator substracts one complex value from other
+	*/
 	Complex operator - (const Complex &);
+	/**@brief Operator sums real value to complex value
+	*/
 	Complex operator + (const double );
+	/**@brief Friend Operator for summinh two complex values
+	*/
 	friend Complex operator + (const double &, const Complex &);
+	/**@brief Operator substracts real value from complex value
+	*/
 	Complex operator - (const double &);
+	/**@brief Friend Operator substracts one complex value from other
+	*/
 	friend Complex operator - (const double &, const Complex &);
+	/**@brief Operator for multiplying one complex value to other
+	*/
 	Complex operator * (const Complex &);
+	/**@brief Operator for multiplying complex value on real value
+	*/
 	Complex operator * (const double &);
+	/**@brief Friend Operator for multiplying two complex values
+	*/
 	friend Complex operator * (const double &, const Complex &);
+	/**@brief Operator divides one complex value by real value
+	*/
 	Complex operator / (const double &);
+	// Some additional operators
 	Complex & operator += (const Complex &);
 	Complex & operator -= (const Complex &);
 	Complex & operator *= (const Complex &);
